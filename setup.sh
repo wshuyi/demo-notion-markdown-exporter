6ab3649e99481bf3fc80f3f49a7d5f6e38b74015
Shuyi Wang <nkwshuyi@gmail.com>
2020-07-16 17:55:17 +0800

    setup.sh added

---
+++ setup.sh
@@ -0,0 +1,13 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"nkwshuyi@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
