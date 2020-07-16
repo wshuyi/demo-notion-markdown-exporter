bcb231a6209e845d722009522cbaea35cb577c21
Shuyi Wang <nkwshuyi@gmail.com>
2020-07-16 20:12:20 +0800

    added github page

--- notion-md-exporter.py
+++ notion-md-exporter.py
@@ -167,7 +167,7 @@ def zipdir(path, ziph):
 # main proc starts here
 st.title("Notion Markdown Exporter")
 st.markdown("This Web app is developed by [Shuyi Wang](https://twitter.com/wshuyi) based on [Eunchan Cho(@echo724)\'s notion2md](https://github.com/echo724/notion2md)")
-
+st.markdown("The coressponding [Github Page of this app is here](https://github.com/wshuyi/demo-notion-markdown-exporter).")
 
 
 token_v2 = st.text_input("Your Notion token_v2:")
